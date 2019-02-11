ed62431bd0d4421873f1c35fd08ba0ac78792167
Alex_imac <amr7@alumni.princeton.edu>
2019-02-11 10:44:02 -0500

    apple

--- body_behavioural_lastrun.py
+++ body_behavioural_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.0b11),
-    on Fri Feb  8 16:45:02 2019
+    on Fri Feb  8 17:15:42 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
