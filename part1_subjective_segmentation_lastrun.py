7e831074af300f488e9c3c5e5c60d040866f7267
Alexandra Reblando <amr7@princeton.edu>
2019-05-04 15:05:04 -0400

    d

--- part1_subjective_segmentation_lastrun.py
+++ part1_subjective_segmentation_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.0.4),
-    on Fri Mar  1 11:14:59 2019
+    on Sat May  4 13:36:37 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
